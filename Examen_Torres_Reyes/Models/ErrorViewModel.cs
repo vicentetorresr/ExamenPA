6f067d327f2c6c2fabc4f21edda1909cded72f20
vicentetorresr <vicentetorres2c@gmail.com>
2024-11-28 20:01:44 -0300

    Agregar archivos de proyecto.

---
+++ Examen_Torres_Reyes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Examen_Torres_Reyes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
